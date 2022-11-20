6e79fcdc87156fb7f7b25797a9216b3ba86fecca
Asad Ali <asadcr@live.com>
2022-11-20 19:09:06 +0500

    adjust entitlements

--- Classes/LinphoneManager.m
+++ Classes/LinphoneManager.m
@@ -82,7 +82,7 @@ NSString *const kLinphoneConfStateParticipantListChanged = @"kLinphoneConfStateP
 NSString *const kLinphoneMagicSearchStarted = @"LinphoneMagicSearchStarted";
 NSString *const kLinphoneMagicSearchFinished = @"LinphoneMagicSearchFinished";
 
-NSString *const kLinphoneMsgNotificationAppGroupId = @"group.org.linphone.phone.msgNotification";
+NSString *const kLinphoneMsgNotificationAppGroupId = @"group.com.synetec.linphone.msgNotification";
 
 const int kLinphoneAudioVbrCodecDefaultBitrate = 36; /*you can override this from linphonerc or linphonerc-factory*/
 
